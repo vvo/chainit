005bdd43968faec94f022f82a756cd1671e61e8e
vvo <vincent.voyer@gmail.com>
2013-10-30 10:59:08 +0100

    add failling test using setTimeouts
    refactor, handle a lot more async/nextTick/nested chains cases
    
    Turns ou I needed to handle one queue per chain depth

--- index.js
+++ index.js
@@ -2,22 +2,54 @@ module.exports = chainit;
 
 function chainit(Constructor) {
   var Queue = require('queue');
-  var q = new Queue({
-    timeout: 0,
-    concurrency: 1
-  });
-  var curIdx = 0;
-  var parent;
+  var queues = [];
+  var currentDepth = 0;
+
+  function pushTo(depth, task) {
+    var queue = queues[depth] || (queues[depth] = getNewQueue(depth));
+    queue.push(task);
+  }
+
+  function getNewQueue(newDepth) {
+    var queue = new Queue({
+      timeout: 0,
+      concurrency: 1
+    });
+
+    queue.on('drain', function() {
+      if (newDepth > 0) {
+        wakeupChain(newDepth);
+      }
+
+      if (!queues.slice(newDepth).some(hasPending)) {
+        currentDepth = newDepth;
+      }
+    });
+
+    function wakeupChain(depth) {
+      if (!queues[depth + 1] ||
+        !queues.slice(depth).some(hasPending)) {
+        queues[depth - 1].concurrency = 1;
+        queues[depth - 1].process();
+      }
+
+      if (depth > 1) {
+        wakeupChain(depth - 1);
+      } else {
+        if (!queues.some(hasPending)) {
+          depth = 0;
+        }
+      }
+    }
+
+    return queue;
+  }
 
   var methods = Object.keys(Constructor.prototype);
   methods.forEach(function(name) {
     var original = Constructor.prototype[name];
 
     var chained = function() {
-      if (parent === true) {
-        q.pending -= 1;
-      }
-
       var ctx = this;
       var args = Array.prototype.slice.call(arguments);
       var customCb;
@@ -25,23 +57,30 @@ function chainit(Constructor) {
         customCb = args.pop();
       }
 
+      var ldepth = currentDepth;
+
+
+      if (currentDepth > 0 && queues[currentDepth - 1].concurrency > 0) {
+        queues[currentDepth - 1].concurrency = 0;
+      }
+
       var task = function(cb) {
-        parent = true;
+        currentDepth = ldepth + 1;
+
         args.push(function() {
-          parent = false;
-          curIdx = q.indexOf(task);
+          var cbArgs = arguments;
+
           if (customCb) {
-            customCb.apply(ctx, arguments);
+            customCb.apply(ctx, cbArgs);
           }
+
           cb();
         });
 
         original.apply(ctx, args);
       }
 
-      q.splice(curIdx + 1, 0, task);
-      curIdx += 1;
-
+      pushTo(currentDepth, task);
       return this;
     }
 
@@ -49,4 +88,8 @@ function chainit(Constructor) {
   });
 
   return Constructor;
+}
+
+function hasPending(queue) {
+  return queue.length >= 1;
 }
